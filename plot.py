9dcdba0e83b8f867c9445e718d04cad70b0dab48
Jonathan Zalger <jzalger@users.noreply.github.com>
2017-10-30 16:41:19 -0400

    Updates for functional variables.

--- plot.py
+++ plot.py
@@ -1,204 +1,170 @@
-import itertools
 import os
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+import sys
+import vae
+import plot
 import numpy as np
+import tensorflow as tf
+from utils import get_mnist
+from tensorflow.examples.tutorials.mnist import input_data
+
+FEATURES = 4  # Number of scalar input features.
+IMG_DIM = 28  # size of one side of the image. Total image variables = IMG_DIM * IMG_DIM
+
+X_LENGTH = FEATURES + IMG_DIM ** 2
+
+ARCHITECTURE = [X_LENGTH,   # total input layer dimension
+                500, 500,   # intermediate encoding layer size - eg: 2 layers of 500 neurons
+                2,          # latent space dims
+                FEATURES]   # length of function features.
+
+HYPERPARAMS = {
+    "batch_size": 128,
+    "learning_rate": 5E-4,
+    "dropout": 0.9,
+    "lambda_l2_reg": 1E-5,
+    "nonlinearity": tf.nn.elu,
+    "squashing": tf.nn.sigmoid
+}
+
+MAX_ITER = 1000
+MAX_EPOCHS = np.inf
+
+# LOG_DIR = "./log"
+# METAGRAPH_DIR = "./out"
+# PLOTS_DIR = "./png"
+METAGRAPH_DIR = r"C:\Users\pw49398\School\CS229\vae-tf-master\vae-tf-master\out"
+LOG_DIR = r"C:\Users\pw49398\School\CS229\vae-tf-master\vae-tf-master\log"
+PLOTS_DIR = r"C:\Users\pw49398\School\CS229\vae-tf-master\vae-tf-master\png"
+
+class DataSet(object):
+    def __init__(self, img_data, nfeatures):
+        self.img_data = img_data
+        self.nfeatures = nfeatures
+        self.epochs_completed = 0
+        self.img_length = IMG_DIM**2
+        self.images = None
+        self.labels = None
+        self._gen_images()
+
+    def next_batch(self, batch_size):
+        x, y = self.img_data.next_batch(batch_size)
+        for _ in range(self.nfeatures):
+            y_noise = np.add(np.random.rand(len(y)), y)
+            x = np.concatenate((x, y_noise.reshape(len(y), 1)), axis=1)
+        return x, y
+
+    def _gen_images(self):
+        x = self.img_data._images
+        y = self.img_data._labels
+        for _ in range(self.nfeatures):
+            y_noise = np.add(np.random.rand(len(y)), y)
+            x = np.concatenate((x, y_noise.reshape(len(y), 1)), axis=1)
+        self.images = x
+        self.labels = y
 
 
-def plotSubset(model, x_in, x_reconstructed, n=10, cols=None, outlines=True,
-               save=True, name="subset", outdir="."):
-    """Util to plot subset of inputs and reconstructed outputs"""
-    n = min(n, x_in.shape[0])
-    cols = (cols if cols else n)
-    rows = 2 * int(np.ceil(n / cols)) # doubled b/c input & reconstruction
-
-    plt.figure(figsize=(cols * 2, rows * 2))
-    dim = int(model.architecture[0]**0.5 - model.architecture[4]) # assume square images
-
-    def drawSubplot(x_, ax_):
-        plt.imshow(x_[0:dim*dim].reshape([dim, dim]), cmap="Greys")
-        if outlines:
-            ax_.get_xaxis().set_visible(False)
-            ax_.get_yaxis().set_visible(False)
-        else:
-            ax_.set_axis_off()
-
-    for i, x in enumerate(x_in[:n], 1):
-        # display original
-        ax = plt.subplot(rows, cols, i) # rows, cols, subplot numbered from 1
-        drawSubplot(x, ax)
-
-    for i, x in enumerate(x_reconstructed[:n], 1):
-        # display reconstruction
-        ax = plt.subplot(rows, cols, i + cols * (rows / 2))
-        drawSubplot(x, ax)
-
-    # plt.show()
-    if save:
-        title = "{}_batch_{}_round_{}_{}.png".format(
-            model.datetime, "_".join(map(str, model.architecture)), model.step, name.replace(">", "_"))
-        plt.savefig(os.path.join(outdir, title), bbox_inches="tight")
+class InputData(object):
+    """Object which combines image variables with functional features."""
+    def __init__(self, nfeatures):
+        self.nfeatures = nfeatures
+        self.mnist = input_data.read_data_sets("./mnist_data")
+        self.train = DataSet(self.mnist.train, nfeatures)
+        self.test = DataSet(self.mnist.test, nfeatures)
+        self.validation = DataSet(self.mnist.validation, nfeatures)
 
 
-def plotInLatent(model, x_in, labels=[], range_=None, title=None,
-                 save=True, name="data", outdir="."):
-    """Util to plot points in 2-D latent space"""
-    assert model.architecture[-1] == 2, "2-D plotting only works for latent space in R2!"
-    title = (title if title else name)
-    mus, _ = model.encode(x_in)
-    ys, xs = mus.T
+def all_plots(model, mnist):
+    if model.architecture[-1] == 2:  # only works for 2-D latent
+        print("Plotting in latent space...")
+        plot_all_in_latent(model, mnist)
 
-    plt.figure()
-    plt.title("round {}: {} in latent space".format(model.step, title))
-    kwargs = {'alpha': 0.8}
+        print("Exploring latent...")
+        plot.exploreLatent(model, nx=20, ny=20, range_=(-4, 4), outdir=PLOTS_DIR)
+        for n in (24, 30, 60, 100):
+            plot.exploreLatent(model, nx=n, ny=n, ppf=True, outdir=PLOTS_DIR,
+                               name="explore_ppf{}".format(n))
 
-    classes = set(labels)
-    if classes:
-        colormap = plt.cm.rainbow(np.linspace(0, 1, len(classes)))
-        kwargs['c'] = [colormap[i] for i in labels]
+    print("Interpolating...")
+    interpolate_digits(model, mnist)
 
-        # make room for legend
-        ax = plt.subplot(111)
-        box = ax.get_position()
-        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-        handles = [mpatches.Circle((0,0), label=class_, color=colormap[i])
-                    for i, class_ in enumerate(classes)]
-        ax.legend(handles=handles, shadow=True, bbox_to_anchor=(1.05, 0.45),
-                    fancybox=True, loc='center left')
+    print("Plotting end-to-end reconstructions...")
+    plot_all_end_to_end(model, mnist)
 
-    plt.scatter(xs, ys, **kwargs)
+    print("Morphing...")
+    morph_numbers(model, mnist, ns=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
 
-    if range_:
-        plt.xlim(*range_)
-        plt.ylim(*range_)
-
-    # plt.show()
-    if save:
-        title = "{}_latent_{}_round_{}_{}.png".format(
-            model.datetime, "_".join(map(str, model.architecture)),
-            model.step, name.replace(">", "_"))
-        plt.savefig(os.path.join(outdir, title), bbox_inches="tight")
+    print("Plotting 10 MNIST digits...")
+    for i in range(10):
+        plot.justMNIST(get_mnist(i, mnist), name=str(i), outdir=PLOTS_DIR)
 
 
-def exploreLatent(model, nx=20, ny=20, range_=(-4, 4), ppf=False,
-                  save=True, name="explore", outdir="."):
-    """Util to explore low-dimensional manifold of latent space"""
-    assert model.architecture[-1] == 2, "2-D plotting only works for latent space in R2!"
-    # linear range; else ppf (percent point function) == inverse CDF from [0, 1]
-    range_ = ((0, 1) if ppf else range_)
-    min_, max_ = range_
-    dim = int(model.architecture[0]**0.5 - model.architecture[4])
-
-    # complex number steps act like np.linspace
-    # row, col indices (i, j) correspond to graph coords (y, x)
-    # rollaxis enables iteration over latent space 2-tuples
-    zs = np.rollaxis(np.mgrid[max_:min_:ny*1j, min_:max_:nx*1j], 0, 3)
-
-    if ppf: # sample from prior ~ N(0, 1)
-        from scipy.stats import norm
-        DELTA = 1E-16 # delta to avoid +/- inf at 0, 1 boundaries
-        zs = np.array([norm.ppf(np.clip(z, DELTA, 1 - DELTA)) for z in zs])
-
-    canvas = np.vstack([np.hstack([x[0:dim*dim].reshape([dim, dim])
-                                   for x in model.decode(z_row)])
-                        for z_row in iter(zs)])
-
-    plt.figure(figsize=(nx / 2, ny / 2))
-    # `extent` sets axis labels corresponding to latent space coords
-    plt.imshow(canvas, cmap="Greys", aspect="auto", extent=(range_ * 2))
-    if ppf: # no axes
-        ax = plt.gca()
-        ax.set_frame_on(False)
-        ax.set_xticks([])
-        ax.set_yticks([])
-        plt.axis("off")
-    plt.tight_layout()
-
-    # plt.show()
-    if save:
-        title = "{}_latent_{}_round_{}_{}.png".format(
-            model.datetime, "_".join(map(str, model.architecture)), model.step, name.replace(">", "_"))
-        plt.savefig(os.path.join(outdir, title), bbox_inches="tight")
+def plot_all_in_latent(model, mnist):
+    names = ("train", "validation", "test")
+    datasets = (mnist.train, mnist.validation, mnist.test)
+    for name, dataset in zip(names, datasets):
+        plot.plotInLatent(model, dataset.images, dataset.labels, name=name,
+                          outdir=PLOTS_DIR)
 
 
-def interpolate(model, latent_1, latent_2, n=20, save=True, name="interpolate", outdir="."):
-    """Util to interpolate between two points in n-dimensional latent space"""
-    zs = np.array([np.linspace(start, end, n) # interpolate across every z dimension
-                    for start, end in zip(latent_1, latent_2)]).T
-    xs_reconstructed = model.decode(zs)
-
-    dim = int(model.architecture[0]**0.5 - model.architecture[4])
-    canvas = np.hstack([x[0:dim*dim].reshape([dim, dim]) for x in xs_reconstructed])
-
-    plt.figure(figsize=(n, 2))
-    plt.imshow(canvas, cmap="Greys")
-    plt.axis("off")
-    plt.tight_layout()
-
-    # plt.show()
-    if save:
-        title = "{}_latent_{}_round_{}_{}".format(
-            model.datetime, "_".join(map(str, model.architecture)), model.step, name.replace(">", "_"))
-        plt.savefig(os.path.join(outdir, title), bbox_inches="tight")
+def interpolate_digits(model, mnist):
+    imgs, labels = mnist.train.next_batch(100)
+    idxs = np.random.randint(0, imgs.shape[0] - 1, 2)
+    mus, _ = model.encode(np.vstack(imgs[i] for i in idxs))
+    plot.interpolate(model, *mus, name="interpolate_{}->{}".format(
+        *(labels[i] for i in idxs)), outdir=PLOTS_DIR)
 
 
-def justMNIST(x, save=True, name="digit", outdir="."):
-    """Plot individual pixel-wise MNIST digit vector x"""
-    DIM = 28
-    TICK_SPACING = 4
-
-    fig, ax = plt.subplots(1, 1)
-    plt.imshow(x[0:DIM*DIM].reshape([DIM, DIM]), cmap="Greys",
-               extent=((0, DIM) * 2), interpolation="none")
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(TICK_SPACING))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(TICK_SPACING))
-
-    # plt.show()
-    if save:
-        title = "mnist_{}.png".format(name.replace(">", "_"))
-        plt.savefig(os.path.join(outdir, title), bbox_inches="tight")
+def plot_all_end_to_end(model, mnist):
+    names = ("train", "validation", "test")
+    datasets = (mnist.train, mnist.validation, mnist.test)
+    for name, dataset in zip(names, datasets):
+        x, _ = dataset.next_batch(10)
+        x_reconstructed = model.vae(x)
+        plot.plotSubset(model, x, x_reconstructed, n=10, name=name,
+                        outdir=PLOTS_DIR)
 
 
-def morph(model, zs, n_per_morph=10, loop=True, save=True, name="morph", outdir="."):
-    """Plot frames of morph between zs (np.array of 2+ latent points)"""
-    assert len(zs) > 1, "Must specify at least two latent pts for morph!"
-    dim = int(model.architecture[0]**0.5 - model.architecture[4]) # assume square images
+def morph_numbers(model, mnist, ns=None, n_per_morph=10):
+    if not ns:
+        import random
+        ns = random.sample(range(10), 10)  # non-in-place shuffle
 
-    def pairwise(iterable):
-        """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
-        # via https://docs.python.org/dev/library/itertools.html
-        a, b = itertools.tee(iterable)
-        next(b, None)
-        return zip(a, b)
+    xs = np.squeeze([get_mnist(n, mnist) for n in ns])
+    mus, _ = model.encode(xs)
+    plot.morph(model, mus, n_per_morph=n_per_morph, outdir=PLOTS_DIR,
+               name="morph_{}".format("".join(str(n) for n in ns)))
 
-    if loop:
-        zs = np.append(zs, zs[:1], 0)
 
-    all_xs = []
-    for z1, z2 in pairwise(zs):
-        zs_morph = np.array([np.linspace(start, end, n_per_morph)
-                             # interpolate across every z dimension
-                             for start, end in zip(z1, z2)]).T
-        xs_reconstructed = model.decode(zs_morph)
-        all_xs.extend(xs_reconstructed)
+def main(to_reload=None):
+    # mnist in a .Dataset object format
+    # mnist = load_mnist()
+    data = InputData(FEATURES)
 
-    for i, x in enumerate(all_xs):
-        plt.figure(figsize=(5, 5))
-        plt.imshow(x[0:dim*dim].reshape([dim, dim]), cmap="Greys")
+    if to_reload:  # restore
+        v = vae.VAE(ARCHITECTURE, HYPERPARAMS, meta_graph=to_reload)
+        print("Loaded!")
 
-        # axes off
-        ax = plt.gca()
-        ax.set_frame_on(False)
-        ax.set_xticks([])
-        ax.set_yticks([])
-        plt.axis("off")
+    else:  # train
+        v = vae.VAE(ARCHITECTURE, HYPERPARAMS, log_dir=LOG_DIR)
+        v.train(data, max_iter=MAX_ITER, max_epochs=MAX_EPOCHS, cross_validate=False,
+                verbose=True, save=True, outdir=METAGRAPH_DIR, plots_outdir=PLOTS_DIR,
+                plot_latent_over_time=False)
+        print("Trained!")
 
-        # plt.show()
-        if save:
-            title = "{}_latent_{}_round_{}_{}.{}.png".format(
-                model.datetime, "_".join(map(str, model.architecture)),
-                model.step, name.replace(">", "_"), i)
-            plt.savefig(os.path.join(outdir, title), bbox_inches="tight")
+    all_plots(v, data)
+
+
+if __name__ == "__main__":
+    tf.reset_default_graph()
+
+    for DIR in (LOG_DIR, METAGRAPH_DIR, PLOTS_DIR):
+        try:
+            os.mkdir(DIR)
+        except(FileExistsError):
+            pass
+
+    try:
+        to_reload = sys.argv[1]
+        main(to_reload=to_reload)
+    except(IndexError):
+        main()
